451204dfc5ec26e556abaac9e1e9af3da34cce0b
Mr_Gamr211 <118116138+MrGamr211@users.noreply.github.com>
2025-11-04 17:32:28 +0300

    Pause menu prefab added
    you can:
    press escape key to pause.
    resume if you want to continue the game.
    restart the same active level.
    return to the main menu.

---
+++ Assets/_Dev/MOHAMMED/_Scripts/Menu_UI/PauseMenu_Functions.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu_Functions : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    //
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && pauseMenu.activeSelf == false)
+        {
+            Pause();
+            Debug.Log("Pressed");
+        }
+    }
+    //
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Home()
+    {
+        SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1;
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+}
